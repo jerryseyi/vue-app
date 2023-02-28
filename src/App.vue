29cc4af74cb3ab8093cd3b768a5c89c8b348ffe6
Jerry Seyi <jerryseyi68@gmail.com>
2023-02-28 20:24:33 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    data() {
+      return {
+        isValid: false,
+        state: 'unverified',
+        counter: 0
+      }
+    },
+    methods: {
+      getStatus () {
+        this.isValid = ! this.isValid;
+      },
+
+      currentState() {
+        this.state = 'verified';
+      },
+
+      incrementCounter() {
+        this.counter++;
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <button @click="getStatus">Click me</button>
+    <br>
+    <h1>{{ isValid }}</h1>
+    <br><br>
+    <button @click="currentState">Get current State</button>
+    <p>The current state is {{state}}</p>
+    <h3>Counter</h3>
+    <p>counter is {{ counter }} <a @click.prevent="incrementCounter">increment counter</a></p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
